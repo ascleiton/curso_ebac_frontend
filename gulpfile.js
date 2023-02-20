f5f19441d25499d57920533289d3de6e52aefd75
Cleiton Souza <cleitonk994@hotmail.com>
2023-02-20 10:09:47 -0300

    compressão dos scripts

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
-const {src, dest} = require("gulp");
+const {src, dest, task} = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const imagemin = require("gulp-imagemin");
+const uglify= require("gulp-uglify");
+
+function comprimeJS(){
+    return src("./source/scripts/*.js")
+    .pipe(uglify())
+    .pipe(dest("./build/scripts"));
+}
 
 exports.default = ()=>{
     return src("./source/styles/main.scss")
@@ -9,8 +16,12 @@ exports.default = ()=>{
     })).on("error", sass.logError)
     .pipe(dest("./build/styles"));
 }
-exports.default = ()=>{
+
+function compilaImagem(){
     return src("./source/images/*")
-    .pipe(imagemin()).on("error",imagemin.logError)
+    .pipe(imagemin())
     .pipe(dest("./build/images"));
 }
+
+exports.script = comprimeJS;
+exports.imagem = compilaImagem;
