f85c380b0a8bad89659011b1bf43e4279e8da322
Cleiton Souza <cleitonk994@hotmail.com>
2023-02-20 08:35:44 -0300

    adicionando imagens ao projeto

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const {src, dest} = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const imagemin = require("gulp-imagemin");
 
 exports.default = ()=>{
     return src("./source/styles/main.scss")
@@ -7,4 +8,9 @@ exports.default = ()=>{
         outputStyle: "compressed"
     })).on("error", sass.logError)
     .pipe(dest("./build/styles"));
+}
+exports.default = ()=>{
+    return src("./source/images/*")
+    .pipe(imagemin()).on("error",imagemin.logError)
+    .pipe(dest("./build/images"));
 }
